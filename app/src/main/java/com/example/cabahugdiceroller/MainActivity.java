06a799c3cda7b5e300fd61a7ba2c9392512e3616
morty-lab <aniejosephcabahug@gmail.com>
2023-10-31 22:20:22 +0800

    added another dice and a random color generator

--- app/src/main/java/com/example/cabahugdiceroller/MainActivity.java
+++ app/src/main/java/com/example/cabahugdiceroller/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.cabahugdiceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -14,42 +17,67 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Random random = new Random();
-    ImageView dice;
+    ImageView dice1,dice2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dice = findViewById(R.id.dice_image);
+        dice1 = findViewById(R.id.dice_image1);
+        dice2 = findViewById(R.id.dice_image2);
         Button roll = findViewById(R.id.roll);
         roll.setOnClickListener(this);
     }
 
+    public Bitmap setImage(){
+        int i = random.nextInt(6)+1;
+        String drawable_name = String.format("dice_%d",i);
+        int imagedrawable = getResources().getIdentifier(drawable_name,"drawable",getPackageName());
+
+        Bitmap image = BitmapFactory.decodeResource(getResources(), imagedrawable);
+
+        Bitmap mutableImage = image.copy(Bitmap.Config.ARGB_8888, true);
+
+        int targetColor = Color.parseColor("#FFFFFF"); // Color you want to change
+
+        int red = random.nextInt(256); // 0-255
+        int green = random.nextInt(256); // 0-255
+        int blue = random.nextInt(256); // 0-255
+
+
+        int replacementColor = Color.argb(255, red, green, blue); // Color to replace with
+
+        for (int x = 0; x < mutableImage.getWidth(); x++) {
+            for (int y = 0; y < mutableImage.getHeight(); y++) {
+                int pixelColor = mutableImage.getPixel(x, y);
+
+                if (pixelColor == targetColor) {
+                    mutableImage.setPixel(x, y, replacementColor);
+                }
+            }
+        }
+
+        return mutableImage;
+    }
+
     @Override
     public void onClick(View view) {
-        int i = random.nextInt(5)+1;
         Animation animation = AnimationUtils.loadAnimation(this,R.anim.rotate);
-        dice.startAnimation(animation);
+        dice1.startAnimation(animation);
+        dice1.setImageBitmap(setImage());
+        dice2.startAnimation(animation);
+        dice2.setImageBitmap(setImage());
 
-        switch(i){
-            case 1:
-                dice.setImageResource(R.drawable.dice1);
-                break;
-            case 2:
-                dice.setImageResource(R.drawable.dice2);
-                break;
-            case 3:
-                dice.setImageResource(R.drawable.dice3);
-                break;
-            case 4:
-                dice.setImageResource(R.drawable.dice4);
-                break;
-            case 5:
-                dice.setImageResource(R.drawable.dice5);
-                break;
-            case 6:
-                dice.setImageResource(R.drawable.dice6);
-                break;
-        }
+
+
+
+
+
+
+
     }
+
+
+
+
 }
